7805eba1e55142ae5af675e540a9cdab23b27bd6
disbeliever <nerevar101@mail.ru>
2014-09-03 20:53:17 +0400

    Created app. Working on models.

---
+++ tracker/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class TicketStatus(models.Model):
+    status = models.CharField(max_length=100)
+
+
+class Ticket(models.Model):
+    project = models.ForeignKey(Project)
+    text = models.CharField(max_length=1500)
+    status = models.ForeignKey(TicketStatus)
+    assigned_user = models.ForeignKey(User)
+    dt_created = models.DateTimeField('date/time created')
+
+
+class TicketHistory(models.Model):
+    ticket = models.ForeignKey(Ticket)
+    dt = models.DateTimeField('date/time')
+    text = models.CharField(max_length=1500)
+    user = models.ForeignKey(User)
