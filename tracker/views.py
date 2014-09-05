61d7c2dfde28f3ae90670fedbed75365cbec7ee4
disbeliever <nerevar101@mail.ru>
2014-09-05 21:44:25 +0400

    Working on template for tickets list

--- tracker/views.py
+++ tracker/views.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
 
 from tracker.models import Ticket
 
 
 def index(request):
     latest_tickets = Ticket.objects.order_by('-dt_created')[:5]
-    output = '<br>'.join([u"{0}: {1}".format(p.id, p.text) for p in latest_tickets])
-    return HttpResponse(output)
+    template = loader.get_template('tracker/index.html')
+    context = RequestContext(request, {
+        'latest_tickets': latest_tickets,
+    })
+    return HttpResponse(template.render(context))
 
 
 def ticket(request, ticket_id):
     try:
-        poll = Ticket.objects.get(pk=ticket_id)
+        ticket = Ticket.objects.get(pk=ticket_id)
     except Ticket.DoesNotExist:
         raise Http404
-    return HttpResponse(u"<p>{0}: {1}".format(poll.id, poll.text))
-    #return render(request, 'polls/detail.html', {'poll': poll})
+    return HttpResponse(u"<p>{0}: {1}".format(ticket.id, ticket.text))
